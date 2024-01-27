36da6a775866549eda8b973265a8213ef9e251e3
Yu <awushu6@gmail.com>
2024-01-27 14:28:04 +0400

    Update react-router-dom version to 6.21.3

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Button = () => {
-  return (
-    <>
-     <div className='flex flex-row gap-4'>
-          <div>
-            <button onClick = {() => {}} className='bg-transperent text-neutral-300 font-medium py-2'>
-              
-              Sign up
-            </button>
-          </div>
-          <div>
-            <button onClick = {() => {}} className='bg-white px-6 py-2 rounded-full'>
-              
-              Login
-            </button>
-          </div>
-        </div>
-    
-    </>
-  )
-}
+  const navigate = useNavigate();
 
-export default Button
+  const handleSignUp = () => {
+    navigate('/signup');
+  };
+
+  const handleLogin = () => {
+    navigate('/login');
+  };
+
+  return (
+    <div className='flex flex-row gap-4'>
+      <div>
+        <button onClick={handleSignUp} className='bg-transparent text-neutral-300 font-medium py-2'>
+          Sign up
+        </button>
+      </div>
+      <div>
+        <button onClick={handleLogin} className='bg-white px-6 py-2 rounded-full'>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Button;
