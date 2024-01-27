9599c58d79825ab49b4e0ad9afe94509fb9e32a4
Yu <awushu6@gmail.com>
2024-01-28 00:38:10 +0400

    Set default country to 'US' and remove unnecessary axios request for geo information

--- client/src/pages/AroundYou.jsx
+++ client/src/pages/AroundYou.jsx
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
 import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
 const CountryTracks = () => {
-  const [country, setCountry] = useState('');
+  const [country, setCountry] = useState('US'); // Set country directly to 'US'
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
-    axios
-      .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
-      .then((res) => setCountry(res?.data?.location.country))
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
+    // No need to make the axios request for geo information
+    setLoading(false);
   }, [country]);
 
   if (isFetching && loading) return <Loader title="Loading Songs around you..." />;
