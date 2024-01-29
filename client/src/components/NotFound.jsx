776fea88fe4f94c8d3ba38e1127504422ba374a6
Yu <awushu6@gmail.com>
2024-01-29 11:53:50 +0400

    Add navigation functionality to NotFound component

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,4 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate("/");
+  };
+
   return (
     <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center flex-col bg-white z-50">
       <img
@@ -6,7 +14,9 @@ const NotFound = () => {
         alt="404 Error"
         className="mb-10"  // Add margin to the bottom for better spacing with text
       />
-     
+      <button onClick={handleRedirect} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Go to Home
+      </button>
     </div>
   );
 };
