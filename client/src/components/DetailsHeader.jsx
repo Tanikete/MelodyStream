22a8d2f67869eda25d34f0c50ada3810993d5b7a
Yu <awushu6@gmail.com>
2024-01-29 13:21:33 +0400

    Update DetailsHeader component styling

--- client/src/components/DetailsHeader.jsx
+++ client/src/components/DetailsHeader.jsx
@@ -5,7 +5,7 @@ const DetailsHeader = ({ artistId, artistData, songData }) => (
   <div className="relative w-full flex flex-col">
     <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
 
-    <div className="absolute inset-0 flex items-center">
+    <div className="absolute inset-0 flex items-center mb-44">
       <img
         alt="profile"
         src={
