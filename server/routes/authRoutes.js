46e208c63bf84b329ca81c5ccdbac23185106f8a
Yu <awushu6@gmail.com>
2024-02-07 19:24:39 +0400

    Update CORS origin in authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,22 +3,17 @@ const router = express.Router();
 const cors = require('cors');
 const { test, registerUser, loginUser, getProfile, logoutUser, resetPassword } = require('../controllers/authController');
 
-
 //middleware
-router.use(cors(
-    {
-        origin: '*',
-        credentials: true,
-        
-    }
-));
+router.use(cors({
+    origin: 'https://melody-stream.vercel.app',
+    credentials: true,
+}));
 
 router.get("/", test)
 router.post("/register", registerUser)
-router.post("/login", loginUser )
+router.post("/login", loginUser)
 router.post('/logout', logoutUser);
 router.get('/profile', getProfile);
 router.post('/reset-password', resetPassword);
-
 
 module.exports = router;
