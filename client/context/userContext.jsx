c3e2d3fc93f82a7f33a413332e360a7f33ece2c4
Yu <awushu6@gmail.com>
2024-02-07 20:49:54 +0400

    Refactor UserContextProvider component

--- client/context/userContext.jsx
+++ client/context/userContext.jsx
@@ -1,20 +1,21 @@
-import axios from 'axios';
-import { createContext, useState, useEffect } from 'react';
-
-export const userContext = createContext({})
-
-export function UserContextProvider({children}) {
+export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
+
     useEffect(() => {
-        if(!user){
-            axios.get('https://melody-stream-server.vercel.app/profile').then(({data}) => {
-                setUser(data);
-        })
-    }
-    }, [])
+        if (!user) {
+            axios.get('https://melody-stream-server.vercel.app/profile')
+                .then(({ data }) => {
+                    setUser(data);
+                })
+                .catch((error) => {
+                    console.error('Error fetching user data:', error);
+                });
+        }
+    }, []);
+
     return (
-        <userContext.Provider value={{user, setUser}}>
+        <userContext.Provider value={{ user, setUser }}>
             {children}
         </userContext.Provider>
-    )
+    );
 }
