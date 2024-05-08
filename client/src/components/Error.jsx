8867a3ed3657841c29f0bb2b21cea57f5d1beb9a
Yu <awushu6@gmail.com>
2024-05-08 09:56:31 +0400

    Update error message to prompt user to search for a song

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Error = () => (
   <div className="w-full flex justify-center items-center">
-    <h1 className="font-bold text-2xl text-white">Something went wrong. Please try again</h1>
+    <h1 className="font-bold text-2xl text-white">Something went wrong. Please search a song</h1>
+    {/* <h1 className="font-bold text-2xl text-white">Something went wrong. Please try again</h1> */}
   </div>
 );
 
